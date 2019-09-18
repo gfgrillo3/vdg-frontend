40c4e33f5c06680df98f531c9dcfff1d5ece1cb4
Gian Franco Grillo <gfgrillo3@gmail.com>
2019-09-18 19:49:01 -0300

    Models para matchear Backend

--- src/app/models/localidad.ts
+++ src/app/models/localidad.ts
@@ -1,12 +1,14 @@
 export class Localidad {
 
-    constructor(idLocalidad=0,nombre='',codigoPostal=''){
+    constructor(idLocalidad=0,nombre='',codigoPostal='',idProvincia=0){
         this.idLocalidad=idLocalidad;
         this.nombre=nombre;
         this.codigoPostal=codigoPostal;
+        this.idProvincia=idProvincia;
     }
 
     idLocalidad: number;
     nombre: string;
     codigoPostal: string;
+    idProvincia: number;
 }
---
+++ src/app/models/provincia.ts
@@ -0,0 +1,9 @@
+export class Provincia {
+    constructor(idProvincia=0,nombre=''){
+        this.idProvincia=idProvincia;
+        this.nombre=nombre;
+    }
+
+    idProvincia: number;
+    nombre: string;
+}
