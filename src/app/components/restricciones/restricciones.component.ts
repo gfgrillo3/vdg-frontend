1ad12aa943aa52e26cc6ebe074f6e867f051b8c1
Gian Franco Grillo <gfgrillo3@gmail.com>
2019-09-19 12:08:03 -0300

    Mapa en Inicio

--- src/app/components/restricciones/restricciones.component.ts
+++ src/app/components/restricciones/restricciones.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RestriccionService } from '../../services/restricciones/restriccion.service';
 import { RestriccionDTO } from 'src/app/models/restriccion-dto';
+declare var ol: any;
 
 @Component({
   selector: 'app-restricciones',
@@ -9,11 +10,26 @@ import { RestriccionDTO } from 'src/app/models/restriccion-dto';
   providers: [RestriccionService]
 })
 export class RestriccionesComponent implements OnInit {
+  latitude: number = -34.522249;
+  longitude: number = -58.700233;
 
+  map: any;
   constructor(private restriccionService : RestriccionService) { }
 
   ngOnInit() {
     this.getRestricciones(localStorage.getItem("emailUsuario"));
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 18
+      })
+    });
   }
 
   getRestricciones(email: string){
