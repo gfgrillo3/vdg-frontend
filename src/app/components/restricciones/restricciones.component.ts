f63b4d3e8dbdc6c3131b7ae543d5b387c849bbd0
Gian Franco Grillo <gfgrillo3@gmail.com>
2019-09-19 13:14:12 -0300

    Cambio codigo de mapa

--- src/app/components/restricciones/restricciones.component.ts
+++ src/app/components/restricciones/restricciones.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RestriccionService } from '../../services/restricciones/restriccion.service';
 import { RestriccionDTO } from 'src/app/models/restriccion-dto';
-declare var ol: any;
+import OlMap from 'ol/Map';
+import OlXYZ from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import OlView from 'ol/View';
+
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-restricciones',
@@ -14,7 +19,10 @@ export class RestriccionesComponent implements OnInit {
   //Latitud y longitud centrada en la UNGS
   latitud: number = -34.522249;
   longitud: number = -58.700233;
-  map: any;
+  map: OlMap;
+  source: OlXYZ;
+  layer: OlTileLayer;
+  view: OlView;
 
   constructor(private restriccionService : RestriccionService) { }
 
@@ -32,17 +40,23 @@ export class RestriccionesComponent implements OnInit {
   }
 
   iniciarMapa(){
-    this.map = new ol.Map({
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([this.longitud, this.latitud]),
+      zoom: 17
+    });
+
+    this.map = new OlMap({
       target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([this.longitud, this.latitud]),
-        zoom: 18
-      })
+      layers: [this.layer],
+      view: this.view
     });
   }
 }
