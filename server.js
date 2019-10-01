6e2539ed9b1a34ad09779924b585e657649b49ff
Gian Franco Grillo <gfgrillo3@gmail.com>
2019-09-30 22:58:10 -0300

    heroku Commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/vdg-frontend'));
+app.use(express.static(__dirname + '/dist/young-cove-92270'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/vdg-frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/young-cove-92270/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
