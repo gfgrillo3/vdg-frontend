f6dac52011612a97ab0b04966584cd856dfe5fd1
gfgrillo3 <gfgrillo3@gmail.com>
2019-10-11 00:50:34 -0300

    metodo getUbicacionRutina

--- src/app/services/ubicaciones/ubicacion.service.ts
+++ src/app/services/ubicaciones/ubicacion.service.ts
@@ -7,7 +7,7 @@ import { UbicacionDto } from 'src/app/models/ubicacion-dto';
 })
 export class UbicacionService {
 
-  readonly URL_API = 'https://vdg-back.herokuapp.com/Ubicacion';
+  readonly URL_API = 'http://localhost:9090/Ubicacion';
 
   constructor(private http: HttpClient) { }
 
@@ -17,6 +17,10 @@ export class UbicacionService {
 
   getEstaInfringiendo(idRestriccion: number, ubicacionDTO: UbicacionDto){
     return this.http.post(this.URL_API + "/infringe/" + idRestriccion, ubicacionDTO);
+  }
+
+  getUbicacionRutinaria(idVictimario: number, dia: number, hora: number, minutos: number){
+    return this.http.get(this.URL_API+"Rutina/persona="+idVictimario+"/dia="+dia+"/hora="+hora+"/minutos="+minutos);
   }
 
 }
