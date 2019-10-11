344a45109449edf30ad340d6b4ef058d02e09809
gfgrillo3 <gfgrillo3@gmail.com>
2019-10-11 01:07:40 -0300

    Map y combo dias para ubicaiconRutina

--- src/app/components/rutinas/rutinas.component.ts
+++ src/app/components/rutinas/rutinas.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import OlMap from 'ol/Map';
+import OlXYZ from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import OlView from 'ol/View';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Icon, Style, Stroke, Fill } from 'ol/style';
+import { Circle } from 'ol/geom';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-rutinas',
@@ -7,11 +18,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutinasComponent implements OnInit {
 
-  dias=['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo',]
+  dias=['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'];
+  minutos=['00','30'];
+
+  //VARIABLES PARA EL MAPA
+  map: OlMap;
+  mapSource: OlXYZ;
+  capaMapa: OlTileLayer;
+  vistaMapa: OlView;
+  vectorUbicaciones: VectorSource;
+  capaUbicaciones: VectorLayer;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.iniciarMapa();
+  }
+
+  iniciarMapa() {
+    //Fuente del mapa,
+    this.mapSource = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+    //Capa para mostrar el mapa
+    this.capaMapa = new OlTileLayer({
+      source: this.mapSource
+    });
+
+    //Centro el mapa en la UNGS O en cualquier lado
+    this.vistaMapa = new OlView({
+      center: fromLonLat([-58.700233, -34.522249]),
+      zoom: 17
+    });
+
+    //Creo el mapa con las capas y la vista
+    this.map = new OlMap({
+      target: 'map',
+      layers: [this.capaMapa],
+      view: this.vistaMapa
+    });
+  }
+
+  getRutina(){
+  //UbicacionService getUbicacionRutina
+  }
+
+  mostrarRutina(){
+    //LLENAR EL MAPA
   }
 
 }
