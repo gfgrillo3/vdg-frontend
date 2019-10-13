41598582e59e5493b43c473add23714eb3c4e697
gfgrillo3 <gfgrillo3@gmail.com>
2019-10-12 22:52:03 -0300

    Mostrar rutinas

--- src/app/components/rutinas/rutinas.component.ts
+++ src/app/components/rutinas/rutinas.component.ts
@@ -13,6 +13,10 @@ import { Vector as VectorLayer } from 'ol/layer';
 import { Icon, Style, Stroke, Fill } from 'ol/style';
 import { Circle } from 'ol/geom';
 import { fromLonLat } from 'ol/proj';
+import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Persona } from 'src/app/models/persona';
+import { UbicacionDto } from 'src/app/models/ubicacion-dto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-rutinas',
@@ -21,9 +25,16 @@ import { fromLonLat } from 'ol/proj';
 })
 export class RutinasComponent implements OnInit {
 
-  dias=['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo',];
+  //VARIABLES PARA LOS PICKER 
+  dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  time: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
+  diaSeleccionado = "Lunes";
+
+  //VARIABLES DE LOGICA
   ubicacionPromedio: Ubicacion;
-  minutos=['00','30'];
+  victimarioSeleccionado: String;
+  seleccioneRestriccion = false;
+  ubicacionesRestriccion: UbicacionDto;
 
   //VARIABLES PARA EL MAPA
   map: OlMap;
@@ -34,12 +45,16 @@ export class RutinasComponent implements OnInit {
   capaUbicaciones: VectorLayer;
 
 
-  constructor(private ubicacionService: UbicacionService, private comunicacion: ComunicacionService) { }
+  constructor(private ubicacionService: UbicacionService, private comunicacion: ComunicacionService,
+    private toastr: ToastrService,) { }
 
   ngOnInit() {
-    //this.getUbicacionPromedio(this.comunicacion.restriccionDTO.victimario.idPersona,1,6,0);
-    this.getUbicacionPromedio(1,1,6,0);
     this.iniciarMapa();
+    if (this.comunicacion.restriccionDTO != null) {
+      var victimario = this.comunicacion.restriccionDTO.victimario;
+      this.victimarioSeleccionado = victimario.apellido + ", " + victimario.nombre;
+      this.seleccioneRestriccion = true;
+    }
   }
 
   iniciarMapa() {
@@ -67,21 +82,67 @@ export class RutinasComponent implements OnInit {
     });
   }
 
-  getRutina(){
-  //UbicacionService getUbicacionRutina
-  }
-
-  mostrarRutina(){
-    //LLENAR EL MAPA
-  }
-
-  getUbicacionPromedio(idPersona: number, dia: number, hora: number, minutos: number) {
-    this.ubicacionService.getUbicacionPromedioRutina(idPersona, dia, hora, minutos)
+  mostrarRutina() {
+    this.ubicacionService.getUbicacionesRestriccion(this.comunicacion.restriccionDTO.restriccion.idRestriccion)
       .subscribe(res => {
-        this.ubicacionPromedio = res as Ubicacion;
-        console.log(res);
-      })
-  }
+        this.ubicacionesRestriccion = res as UbicacionDto;;
+        this.ubicacionService.getUbicacionPromedioRutina(this.comunicacion.restriccionDTO.victimario.idPersona, 
+          this.dias.indexOf(this.diaSeleccionado) + 1, this.time.hour, this.time.minute)
+        .subscribe(res => {
+            this.ubicacionPromedio = res as Ubicacion;
+            //Si no hay ubicación promedio NULL
+            if(this.ubicacionPromedio==null){
+              this.toastr.error("No hay ubicación rutinaria para los datos seleccionados");
+              return;
+            }
 
+            var markerVictimario: Feature;
+            var perimetro: Feature;
+
+            //Marco Victimario con su marker La longitud y latitud es de objeto Ubicacion
+            markerVictimario = new Feature({
+              geometry: new Point(fromLonLat([this.ubicacionesRestriccion.ubicacionVictimario.longitud,
+              this.ubicacionesRestriccion.ubicacionVictimario.latitud]))
+            });
+            markerVictimario.setStyle(new Style({
+              image: new Icon(({
+                src: 'assets/markerVictimario.png',
+                imgSize: [60, 60]
+              }))
+            }));
+
+            //Dibujo Perimetro de ubicacionRutinaria con el estilo
+            perimetro = new Feature();
+            var forma = new Circle(fromLonLat([this.ubicacionPromedio.longitud, this.ubicacionPromedio.latitud]));
+            forma.setRadius(200);
+            perimetro.setGeometry(forma);
+            perimetro.setStyle(new Style({
+              fill: new Fill({
+                color: ([10, 20, 255, .6])
+              })
+            }));
+
+            //Borro lo dibujado anteriormente en el mapa
+            var layers = this.map.getLayers().getArray();
+            for (var i = layers.length - 1; i >= 1; --i) {
+              var layer = layers[i];
+              this.map.removeLayer(layer);
+            }
+
+            //Creo el vector y capa para mostrar Area rutinaria y la ubicacion
+            this.vectorUbicaciones = new VectorSource({
+              features: [markerVictimario, perimetro]
+            });
+            this.capaUbicaciones = new VectorLayer({
+              source: this.vectorUbicaciones
+            });
+
+            //CENTRO EL MAPA EN LA UBICACION VICTIMARIO Y AÑADO LA CAPA
+            this.vistaMapa.setCenter(fromLonLat([this.ubicacionesRestriccion.ubicacionVictimario.longitud,
+            this.ubicacionesRestriccion.ubicacionVictimario.latitud]));
+            this.map.addLayer(this.capaUbicaciones);
+          });
+      });
+  }
 
 }
